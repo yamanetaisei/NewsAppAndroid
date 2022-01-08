bb4c2cd7f405920bcc8a46e93f03de4952be370e
yamanetaisei <swiftmantaisei@gmail.com>
2022-01-08 18:11:56 +0900

    エントリポイントの変更

---
+++ app/src/main/java/com/example/newsappandroid/view/ArticleListActivity.kt
@@ -0,0 +1,12 @@
+package com.example.newsappandroid.view
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.example.newsappandroid.R
+
+class ArticleListActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_article_list)
+    }
+}
