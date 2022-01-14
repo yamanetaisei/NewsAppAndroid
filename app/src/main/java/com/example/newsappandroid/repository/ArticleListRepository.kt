bcd2e4d6e8165ae41311c812d6ea65c8ef26e487
yamanetaisei <swiftmantaisei@gmail.com>
2022-01-15 01:08:23 +0900

    keyを隠した

--- app/src/main/java/com/example/newsappandroid/repository/ArticleListRepository.kt
+++ app/src/main/java/com/example/newsappandroid/repository/ArticleListRepository.kt
@@ -2,13 +2,14 @@ package com.example.newsappandroid.repository
 
 import com.example.newsappandroid.data.ApiClient.retrofit
 import com.example.newsappandroid.data.HeadlinesApiRequest
+import com.example.newsappandroid.data.Key
 import com.example.newsappandroid.model.NewsData
 import io.reactivex.Single
 import io.reactivex.android.schedulers.AndroidSchedulers
 import io.reactivex.schedulers.Schedulers
 
 class ArticleListRepository {
-    val key = "30d06e4f9a934402a204fa89f9d9acfc"
+    private val key = Key().newsApi
     fun fetchArticles(country: String, category: String): Single<NewsData> {
         return retrofit.create(HeadlinesApiRequest::class.java).fetch(country, category, key)
             .subscribeOn(Schedulers.io())
